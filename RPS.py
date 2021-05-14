8abe65d5bc45d1a40c0f953e578d796e3657ffbc
em-ariadne <82678022+em-ariadne@users.noreply.github.com>
2021-05-13 21:46:28 -0500

    Add files via upload

---
+++ RPS.py
@@ -0,0 +1,5 @@
+import random
+user_action = input("Enter a Choice Rock, Paper, or Scissors")
+possible_actions = ["rock", "paper", "scissors"]
+computer_action = random.choice(possible_actions)
+print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")
